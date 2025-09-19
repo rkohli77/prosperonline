cd3087550ede177b48edc30cb11d8af3f3486bd7
Ritu Kohli <rkohli77@gmail.com>
2025-09-18 23:54:55 -0400

    Add dist/ to gitignore and add script from hubspot to connect contact form

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,14 @@ import Services from '@/components/Services';
 import About from '@/components/About';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
+<div> 
+<script>
+  var _hsq = window._hsq = window._hsq || [];
+  _hsq.push(['setPath', '/home']);
+</script>
 
+<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/{hubId}.js"></script>
+</div>
 const Index = () => {
   return (
     <div className="min-h-screen">
